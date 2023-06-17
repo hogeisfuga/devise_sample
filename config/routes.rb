bf286a3ebb8b78008a2c99f886ea0b1954274f33
hogeisfuga <120ce066@gmail.com>
2023-06-18 00:39:48 +0900

    adminページにdevise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
   namespace :admin do
+    # パスとアクション両方にusersをつけたいのでこう書く
+    namespace :staffs do
+      # devise_forについてわからなくなった時は以下参照
+      # https://www.rubydoc.info/github/heartcombo/devise/main/ActionDispatch/Routing/Mapper%3Adevise_for
+      devise_for :staffs, path: ''
+    end
     root to: 'home#top'
   end
-  devise_for :users, controllers: {
-    # オーバーライドしたいアクションを書いていく
-    sessions: 'users/sessions'
-  }
+
+  # パスとアクション両方にusersをつけたいのでこう書く
+  namespace :users do
+    devise_for :users, path: ''
+  end
+
+  # 必要に応じてオーバーライドしたい場合はアクションを書いていく
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
   get 'dashboard' => 'dashboard#index'
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
